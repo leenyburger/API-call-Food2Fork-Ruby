061b8a017bc72d8f3b24dedd276e99f56749bc0e
Colleen Schnettler <bitmappeddesigns@gmail.com>
2016-12-30 12:35:44 -0500

    fixed API call due to space issue

--- module3_1_assignment.rb
+++ module3_1_assignment.rb
@@ -9,17 +9,23 @@ class Recipe
     include HTTParty
     ENV["FOOD2FORK_KEY"] = "5b6b74c6cc0fa9dc23871a7ae753f6c7"
 
-    base_uri 'https://food2fork.com/api' #Same across most requests 
-    default_params fields: "image_url, source_url, title", key: ENV["FOOD2FORK_KEY"] #defaults, like API developer key 
+    base_uri "https://food2fork.com/api" #Same across most requests 
+    default_params key: ENV["FOOD2FORK_KEY"] #defaults, like API developer key #fields: "image_url, source_url, title",
     format :json #Tell it which format data comes in
     #q:"search" request parameter 
 
-    def self.for term 
+    def self.for (keyword) 
     	#class method called for which takes in a term 
-    	get ("/search", query:{q:term})["recipes"]#get is method provided by HTTParty 
-		#elements is the key of the hash from coursera documentation
-		#returns array where each element in the array is a hash 
-	end 
-pp Recipe.for "chocolate"
+    	get("/search", query: {q: keyword})["recipes"]  #get is method provided by HTTParty 
 
+        #elements is the key of the hash from coursera documentation
+        #returns array where each element in the array is a hash 
+	end 
+    pp Recipe.for "chocolate"
 end
+
+
+
+
+
+
